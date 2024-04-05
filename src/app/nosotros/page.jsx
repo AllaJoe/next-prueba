6e0d7984c1f748a79a526a8b6ef02e1deeeba30a
AllaJoe <freddorlost45@gmail.com>
2024-04-05 03:07:58 -0300

    prueba 3

--- src/app/nosotros/page.jsx
+++ src/app/nosotros/page.jsx
@@ -6,13 +6,9 @@ const Nosotros = () => {
     return (
         <div>
            <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
-           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
+           <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus earum, vero aliquid repellendus corporis eveniet fuga vel eius voluptatem tenetur culpa, accusantium dolorum at asperiores non commodi amet voluptatum perspiciatis!0</p>
+           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla aspernatur sint explicabo libero voluptas, adipisci deleniti blanditiis laudantium! Voluptas ratione quaerat consequatur dicta eum et veritatis facilis sunt laudantium quis.</p>
+           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum nesciunt reiciendis nostrum animi id, ipsam provident amet expedita enim corrupti laudantium nihil, porro praesentium, a quis et temporibus adipisci voluptates!</p>
         </div>
     );
 };
