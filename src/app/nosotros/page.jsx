0817968e08ccd9ed28a70dd2b5615ad530a082c6
Damshoegazer <damianheredia90@gmail.com>
2024-04-05 00:58:43 -0300

    esto es una prueba de commit

---
+++ src/app/nosotros/page.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+const Nosotros = () => {
+    return (
+        <div>
+           <h1 className="text-xxl">ESTO ES NOSOTROS</h1> 
+        </div>
+    );
+};
+
+
+
+export default Nosotros;
